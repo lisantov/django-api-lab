9a6eac8e6395b05d3526cdf7844658a6f620c600
lisantov <lisantov@yandex.ru>
2025-12-19 17:03:04 +0700

    Fixed routes, added view for BookList. Created custom permission rule

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .permissions import IsStaffOrReadOnly
 
-# Create your views here.
+from api.models import Book
+from api.serializers import BookSerializer
+
+class BookList(APIView):
+    permission_classes = [IsStaffOrReadOnly]
+    def get(iself, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
